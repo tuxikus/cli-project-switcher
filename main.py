93cd548a64d0b4348c2289ae3ce6e67d388a9a50
tuxikus <contact@tuxikus.de>
2025-03-25 21:37:29 +0100

    Changed variable name of config file and removed unnecessary vars

--- main.py
+++ main.py
@@ -2,26 +2,26 @@
 
 from pathlib import Path
 
-PROJECT_FILE = Path.home() / ".config" / "cli-project-switcher" / "projects"
+PROJECT_CONFIG_FILE = Path.home() / ".config" / "cli-project-switcher" / "projects"
 
-def check_project_file():
-    if not PROJECT_FILE.is_file():
+def check_project_config_file():
+    if not PROJECT_CONFIG_FILE.is_file():
         raise FileNotFoundError()
 
 def read_projects():
-    content = ""
-    
-    with open(PROJECT_FILE, "r") as f:
+    with open(PROJECT_CONFIG_FILE, "r") as f:
         content = f.read()
 
     return content.split("\n")[:-1]
 
 def main():
-    check_project_file()
-    
-    projects = read_projects()
+    try:
+        check_project_config_file()
+    except:
+        print("~/.config/cli-project-switcher/config not found")
+        exit(1)
 
-    for project in projects:
+    for project in read_projects():
         # bash does not recognize ~ in a script
         print(project.replace("~", str(Path.home())).strip())
 
