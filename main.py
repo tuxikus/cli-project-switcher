c7a4ee5e124474ceb4d7bc2665d7f9a1ae3a4953
tuxikus <contact@tuxikus.de>
2025-04-07 20:33:05 +0200

    easy config

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 
 from pathlib import Path
+from glob import glob
 
-PROJECT_CONFIG_FILE = Path.home() / ".config" / "cli-project-switcher" / "projects"
+PROJECT_CONFIG_FILE = Path.home() / ".config" / "cli-project-switcher" / "config"
 
 def check_project_config_file():
     if not PROJECT_CONFIG_FILE.is_file():
         raise FileNotFoundError()
 
-def read_projects():
+def read_project_dirs():
     with open(PROJECT_CONFIG_FILE, "r") as f:
         content = f.read()
 
@@ -18,12 +19,15 @@ def main():
     try:
         check_project_config_file()
     except:
-        print("~/.config/cli-project-switcher/config not found")
+        print(f"{PROJECT_CONFIG_FILE} not found")
         exit(1)
 
-    for project in read_projects():
-        # bash does not recognize ~ in a script
-        print(project.replace("~", str(Path.home())).strip())
+    for project_dir in read_project_dirs():
+        try:
+            for project in Path(project_dir.replace("~", str(Path.home()))).iterdir():
+                print(project)
+        except:
+            pass
 
 if __name__ == "__main__":
     main()
